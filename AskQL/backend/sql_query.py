fa258115efebac53d9b548e048968388e942160f
Sahil <sahilbodke10@gmail.com>
2024-11-07 13:23:33 +0530

    change model

--- AskQL/backend/sql_query.py
+++ AskQL/backend/sql_query.py
@@ -40,7 +40,7 @@ def get_sql_chain(db: SQLDatabase):
     """
 
     prompt = ChatPromptTemplate.from_template(template)
-    llm = ChatGroq(model="mixtral-8x7b-32768", temperature=0)
+    llm = ChatGroq(model="llama3-8b-8192", temperature=0)
     
     def get_schema(_):
         return db.get_table_info()
